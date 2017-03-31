95a2cbfe234d5385083c7a20dd4e942aa9a8279a
Jo Pearce <jo@jopearce.co.uk>
2017-03-31 18:10:38 +0100

    Added @ultimate/aot-loader (and html-loader), also upgraded to ng4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,48 @@
- module.exports = {
-     entry: './out-tsc/main.js',
-     output: {
-         path: './build',
-         filename: 'main.bundle.js'
-     }
- };
+const aotLoader = require('@ultimate/aot-loader');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: ['./src/main.ts']
+    },
+    output: {
+        filename: '[name].js',
+        publicPath: '/',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader", 
+                    options: {
+                        minimize: true
+                    }
+                }]
+            },
+            {
+                test: /\.ts$/,
+                loaders: ['@ultimate/aot-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)@angular/,
+            path.resolve(__dirname, '../src')
+        ),
+        new aotLoader.AotPlugin({
+            entryModule: './src/app/app.module#AppModule',
+            tsConfig: './tsconfig.json'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
